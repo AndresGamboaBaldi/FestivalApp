0897e83c2d2f3d3464f83a23d1a8b0832c0a0657
Andres Gamboa Baldi <andresgamboabaldi@gmail.com>
2021-06-29 16:35:42 -0400

    Adding Footer and Schedule Cards componentes

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,44 +1,60 @@
 <template>
-  <v-footer>
+  <v-footer id="fest-footer">
     <v-card
       flat
-      tile
-      id="footer"
+      tile 
+      id="fest-card"
     >
-      <v-row>
-        <v-col
-          v-for="title in titles"
-          :key="title"
-        >
-          {{ title }}
+      <v-row >
+        <v-col class= "fest-coltitles">
+          {{ titles[0] }}
           <v-row
-          v-for="title in novedades"
-          :key="title"
-          >
-          {{ title }}
+          v-for="text in novedades"
+          :key="text">
+          {{ text }}
+          </v-row>
+        </v-col>
+        <v-col class= "fest-coltitles">
+          {{ titles[1] }}
+          <v-row
+          v-for="text in cronograma"
+          :key="text">
+          {{ text }}
+          </v-row>
+        </v-col>
+        <v-col class= "fest-coltitles">
+          {{ titles[2] }}
+          <v-row
+          v-for="text in tienda"
+          :key="text">
+          {{ text }}
+          </v-row>
+        </v-col>
+        <v-col class= "fest-coltitles" id="fest-lastcol">
+          {{ titles[3] }}
+          <v-row
+          v-for="text in informacion"
+          :key="text">
+          {{ text }}
           </v-row>
         </v-col>
       </v-row>
 
-      <v-divider></v-divider>
+      <v-divider ></v-divider>
 
       <v-card-text>
-        <v-btn
-          v-for="icon in icons"
-          :key="icon"
-          class="mx-4 white--text"
-          icon
-        >
-          <v-icon size="24px">
+          <v-icon size="24px"
+            v-for="icon in icons"
+            :key="icon"
+            class="mx-4 white--text">
             {{ icon }}
           </v-icon>
-        </v-btn>
       </v-card-text>
-
     </v-card>
   </v-footer>
 </template>
 <script>
+import "./Footer.sass"
   export default {
     name: 'HelloWorld',
 
@@ -47,7 +63,7 @@
       novedades: ['Proximos Eventos', 'Artistas', 'Antiguos Eventos'],
       cronograma: ['Viernes', 'Sabado', 'Domingo', 'Lunes'],
       tienda: ['Poleras', 'Sombreros', 'Accesorios', 'Otros'],
-      informacion: ['Terminos y Condiciones', 'Contactos', 'Politica de Privacidad'],
+      informacion: ['Termi y Condiciones', 'Contactos', 'Privacidad'],
       icons: [
         'mdi-facebook',
         'mdi-twitter',
@@ -57,11 +73,4 @@
     }),
   }
 </script>
-<style lang="sass">
-  #footer 
-    height : 320px 
-    width : 100%
-    background-color:  var(--v-primary700)
-    text-align: center
- 
-</style>
+
---
+++ src/components/ScheduleCard/ScheduleCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card class="fest-schedule-card" img="https://e00-marca.uecdn.es/assets/multimedia/imagenes/2020/12/28/16091698032437.jpg">
+    <v-card-title class="fest-schedule-card">
+      Viernes
+    </v-card-title>
+  </v-card>
+</template>
+<script>
+import "./ScheduleCard.sass"
+  export default {
+  name: 'ScheduleCard',
+  data(){
+    return{
+      image:[
+        {src:"https://catalog.21buttons.com/e9f3b44504e17c16049e36c6fa480c72a7aa4ab1.smedium.jpg"},
+        {src:"https://i.pinimg.com/736x/83/16/14/8316146c5a95c15a763ac4fcefc95608.jpg"},
+        {src:"../../assets/cardDomingo.png"},
+      ]
+    }
+  }
+}
+</script>
+
