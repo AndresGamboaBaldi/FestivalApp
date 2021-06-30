885756320c399a14e2603c5f2f36e58c0eed7922
Andres Gamboa Baldi <andresgamboabaldi@gmail.com>
2021-06-29 21:30:54 -0400

    Solving Schedule Card Component

--- src/App.vue
+++ src/App.vue
@@ -5,47 +5,33 @@
       <ticket-card/>
       <events-card/>
       <news-card/>
-      <Carousel/>
-      <!--
-      <Store-card/>
-      <Hello-world/>
-      <ScheduleCard/>
+      <carousel/>
+      <schedule-card/>
       <Footer/>
-      -->
+      <schedule-card  v-for="card in schedulecards"
+          :key="card.id"
+          :imagen="card.imagen" 
+          :texto="card.texto"></schedule-card>    
     </v-main>
   </v-app>
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld';
-//import Footer from './components/Footer/Footer';
-//import HelloWorld from './components/HelloWorld';
+
 import StoreCard from './components/StoreCard/StoreCard.vue';
-//import ScheduleCard from './components/ScheduleCard/ScheduleCard.vue';
-//import HelloWorld from './components/HelloWorld';
-//import Footer from './components/Footer/Footer.vue';
-//import HelloWorld from './components/HelloWorld';
-//<store-card/>
-//<ticket-card/>
-//<events-card/>
+import ScheduleCard from './components/ScheduleCard/ScheduleCard.vue';
 import TicketCard from './components/TicketCard/TicketCard.vue';
 import EventsCard from './components/EventsCard/EventsCard.vue'
 import NewsCard from './components/NewsCard/NewsCard.vue'
 import Carousel from './components/Carousel/Carousel.vue';
-//import HelloWorld from './components/HelloWorld';
-//import Footer from './components/Footer/Footer.vue';
-
-//import StoreCard from './components/StoreCard/StoreCard.vue';
+import Footer from './components/Footer/Footer.vue';
 
 export default {
   name: 'App',
 
   components: {
-    //HelloWorld,
-    //Footer,
-    //ScheduleCard,
-    //HelloWorld,
-    //Footer,
+    ScheduleCard,
+    Footer,
     StoreCard,
     TicketCard,
     EventsCard,
@@ -63,6 +49,19 @@ export default {
   },
 
   data: () => ({
+    schedulecards: [
+      {
+        id: 1,
+        imagen: "https://e00-marca.uecdn.es/assets/multimedia/imagenes/2020/12/28/16091698032437.jpg",
+        texto: "Viernes" 
+      },
+       {
+        id:2,
+        imagen:  "https://www.brusselstimes.com/wp-content/uploads/2019/04/k2_items_src_885112323029b4ff67c59e5af71d2a8a.jpg",
+        texto: "Sabado" 
+      }
+    ],
+    
     //
   }),
 };
--- src/components/ScheduleCard/ScheduleCard.vue
+++ src/components/ScheduleCard/ScheduleCard.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-card class="fest-schedule-card" img="https://e00-marca.uecdn.es/assets/multimedia/imagenes/2020/12/28/16091698032437.jpg">
+  <v-card class="fest-schedule-card" :img="imagen">
     <v-card-title class="fest-schedule-card">
-      Viernes
+      {{texto}}
     </v-card-title>
   </v-card>
 </template>
@@ -9,15 +9,21 @@
 import "./ScheduleCard.sass"
   export default {
   name: 'ScheduleCard',
-  data(){
-    return{
-      image:[
-        {src:"https://catalog.21buttons.com/e9f3b44504e17c16049e36c6fa480c72a7aa4ab1.smedium.jpg"},
-        {src:"https://i.pinimg.com/736x/83/16/14/8316146c5a95c15a763ac4fcefc95608.jpg"},
-        {src:"../../assets/cardDomingo.png"},
-      ]
-    }
-  }
+  props: {
+    imagen: 
+      {
+      type: String,
+      default: "https://e00-marca.uecdn.es/assets/multimedia/imagenes/2020/12/28/16091698032437.jpg"
+      },
+    texto: 
+      {
+      type: String,
+      default: "Viernes"
+      }
+    },
+  data: () => ({
+      img: "https://e00-marca.uecdn.es/assets/multimedia/imagenes/2020/12/28/16091698032437.jpg"
+    }),
 }
 </script>
 
