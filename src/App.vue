ba4382f0477762377326d8654bb67a789d6ec406
Ricardo <ricardo.fernandez.bo.2000@ieee.org>
2021-06-29 22:47:33 -0400

    separar carousel long de carousel large

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <v-app :style="cssProps">
     <v-main>
+      <!--
       <store-card/>
       <ticket-card/>
       <events-card/>
       <news-card/>
-      <Carousel/>
-      <!--
+      <CarouselLarge/>
+      <CarouselLong/>
       <Store-card/>
       <Hello-world/>
       <ScheduleCard/>
@@ -17,40 +18,34 @@
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld';
 //import Footer from './components/Footer/Footer';
-//import HelloWorld from './components/HelloWorld';
-import StoreCard from './components/StoreCard/StoreCard.vue';
 //import ScheduleCard from './components/ScheduleCard/ScheduleCard.vue';
-//import HelloWorld from './components/HelloWorld';
 //import Footer from './components/Footer/Footer.vue';
-//import HelloWorld from './components/HelloWorld';
 //<store-card/>
 //<ticket-card/>
 //<events-card/>
+/*
+import StoreCard from './components/StoreCard/StoreCard.vue';
 import TicketCard from './components/TicketCard/TicketCard.vue';
 import EventsCard from './components/EventsCard/EventsCard.vue'
 import NewsCard from './components/NewsCard/NewsCard.vue'
-import Carousel from './components/Carousel/Carousel.vue';
-//import HelloWorld from './components/HelloWorld';
-//import Footer from './components/Footer/Footer.vue';
-
-//import StoreCard from './components/StoreCard/StoreCard.vue';
-
+import CarouselLarge from './components/Carousel/CarouselLarge.vue';
+import CarouselLong from './components/Carousel/CarouselLong.vue';
+*/
 export default {
   name: 'App',
 
   components: {
-    //HelloWorld,
-    //Footer,
-    //ScheduleCard,
-    //HelloWorld,
-    //Footer,
-    StoreCard,
-    TicketCard,
-    EventsCard,
-    NewsCard,
-    Carousel
+    /*
+      Footer,
+      ScheduleCard,
+      StoreCard,
+      TicketCard,
+      EventsCard,
+      NewsCard,
+      CarouselLarge,
+      CarouselLong
+    */
   },
   computed: {
    cssProps () {
---
+++ src/components/LoginView/LoginView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    a
+  </div>  
+</template>
+
+<script>
+  import "./LoginView.sass"
+  export default {
+    name: 'LoginView',
+
+    data () {
+      return {
+        carousel_data: [
+          {
+            src: 'https://headbangersla.com/wp-content/uploads/2020/12/Conciertos1.jpg',
+            title: 'Next Events',
+            comment: 'Discover the festivals you are the most interested',
+          },
+          {
+            src: 'https://www.pariszigzag.fr/wp-content/uploads/2019/04/festival-paris-zigzag-e1555594557490.jpg',
+            title: 'Live the Experience',
+            comment: 'Listen to your favorite artists',
+          },
+          {
+            src: 'https://scontent.fvvi1-1.fna.fbcdn.net/v/t1.6435-9/144199939_2725547287755891_5580509566480479588_n.jpg?_nc_cat=107&ccb=1-3&_nc_sid=e3f864&_nc_ohc=Yduh3JA9ao8AX9oNkdV&tn=QavYgOEX-JcdHB9n&_nc_ht=scontent.fvvi1-1.fna&oh=6a36c91fcceaad9073e3ca780d6137df&oe=60E00483',
+            title: 'Madness Festival ',
+            comment: 'Discover the artists that will play',
+          },
+        ],
+      }
+    },
+  }
+</script>
